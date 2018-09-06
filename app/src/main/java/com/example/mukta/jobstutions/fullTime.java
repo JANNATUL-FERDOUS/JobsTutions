dfb628ddd7329c24ca662d85711f07bbb4d8f627
Jannatul Ferdous <muktamist2015@gmail.com>
2018-09-05 22:17:58 -0700

    initial commit

---
+++ app/src/main/java/com/example/mukta/jobstutions/fullTime.java
@@ -0,0 +1,17 @@
+package com.example.mukta.jobstutions;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fullTime extends Fragment {
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View myview=inflater.inflate(R.layout.activity_fulltime_main,container,false);
+        return myview;
+    }
+}
